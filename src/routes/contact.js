bb78416689ab99671edc527f806eb00786ed0bf3
rajan-soni <lodhiyarajan@gmail.com>
2022-09-22 00:16:03 +0530

    feat: error page

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -3,9 +3,17 @@ import { Router } from 'express';
 const router = new Router();
 
 router.get('/', (req, res, next) => {
-  res.render("pages/contact",{
-    title: "Contact Us",
-  })
+  res.render('pages/contact', {
+    title: 'Contact Us',
+  });
+});
+
+router.post('/submit-form', (req, res, next) => {
+  throw new Error('TEst');
+  // TODO: add email service here
+  res.render('pages/not-found', {
+    title: 'Submitted',
+  });
 });
 
 export default router;
